5f81ed9b3cb27c9e8c6d7c0fbb10040c549e77d7
Yumeng Ji <yumeng.ji103585@gmail.com>
2025-11-05 00:36:03 +0100

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Graph {
+
+    HashMap<String, Integer> th;
+    String[] keys;
+    ArrayList<Integer>[] adjList;
+
+    public void crearGrafo(UnorderedDoubleLinkedList<Autor> lista){
+        // Post: crea el grafo desde la lista de autores
+        //       Los nodos son nombres de autores
+
+        // Paso 1: llenar th�
+        // COMPLETAR CÓDIGO
+
+        // Paso 2: llenar keys�
+        keys = new String[th.size()];
+        for (String k: th.keySet()) keys[th.get(k)] = k;
+
+        // Paso 3: llenar adjList�
+        // COMPLETAR CÓDIGO
+    }
+
+    public void print(){
+        for (int i = 0; i < adjList.length; i++){
+            System.out.print("Element: " + i + " " + keys[i] + " --> ");
+            for (int k: adjList[i])  System.out.print(keys[k] + " ### ");
+
+            System.out.println();
+        }
+    }
+
+    public boolean estanConectados(String a1, String a2){
+        Queue<Integer> porExaminar = new LinkedList<Integer>();
+
+        int pos1 = th.get(a1);
+        int pos2 = th.get(a2);
+        boolean enc = false;
+        boolean[] examinados = new boolean[th.size()];
+
+        // COMPLETAR CÓDIGO
+
+        return enc;
+
+    }
+
+    public ArrayList<String> estanConectados(String a1, String a2){
+
+        // COMPLETAR CÓDIGO
+
+        return null;
+
+    }
+
+}
