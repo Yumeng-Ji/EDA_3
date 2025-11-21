e928ce190615778f674a958f47ac83b55e31372c
Samira034 <samira.tamang247@gmail.com>
2025-11-21 16:39:33 +0100

    Implement graph creation with co-authorship connections
    Added logic to create graph connections based on co-authorship from the list of authors and their publications.

--- Graph.java
+++ Graph.java
@@ -13,21 +13,53 @@ public class Graph {
         // Post: crea el grafo desde la lista de autores
         //       Los nodos son nombres de autores
 
-        // Paso 1: llenar th�
-        // COMPLETAR CÓDIGO
+                // Paso 1: llenar th�
+        // COMPLETAR C�DIGO
         th = new HashMap<>();
         for(int i = 0; i < lista.size(); i++)
             th.put(lista.get(i).getNombre(), i);
 
-        // Paso 2: llenar keys�
+
+        // Paso 2: llenar keys
         keys = new String[th.size()];
         for (String k: th.keySet()) keys[th.get(k)] = k;
 
         // Paso 3: llenar adjList�
-        // COMPLETAR CÓDIGO
+        // COMPLETAR C�DIGO
         adjList = new ArrayList[th.size()];
         for (int i = 0; i < adjList.length; i++) adjList[i] = new ArrayList<>();
-        // Hay que asignar las relaciones de co-autor y de citas 
+
+        // Construir relaciones basadas en co-autoría
+         HashMap<String, Autor> listaAutores= Repositorio.getRepositorio().getListaAutores();
+        for (Autor autor : lista) {
+            int indiceAutor = th.get(autor.getNombre());
+
+            // Obtener todas las publicaciones del autor
+            UnorderedDoubleLinkedList<Publicacion> publicaciones = autor.getListaPublicaciones();
+
+            // Para cada publicacion, obtener los co-autores
+            for (Publicacion pub : publicaciones) {
+                UnorderedDoubleLinkedList<Autor> autoresPub = pub.getListaAutores();
+
+                // Crear conexiones entre todos los autores de esta publicacion
+                for (Autor coAutor : autoresPub) {
+                    if (!coAutor.getNombre().equals(autor.getNombre())) {
+                        if (th.containsKey(coAutor.getNombre())) {
+                        int indiceCoAutor = th.get(coAutor.getNombre());
+
+                            // Añadir conexion bidireccional
+                            if (!adjList[indiceAutor].contains(indiceCoAutor)) {
+                                adjList[indiceAutor].add(indiceCoAutor);
+                            }
+                            if (!adjList[indiceCoAutor].contains(indiceAutor)) {
+                                adjList[indiceCoAutor].add(indiceAutor);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
     public void print(){
@@ -63,3 +95,4 @@ public class Graph {
 
 
 }
+
