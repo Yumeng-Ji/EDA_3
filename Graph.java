6b4c8216434ced1cdd24deca72c348d24afe01a0
Yumeng Ji <yumeng.ji103585@gmail.com>
2025-11-22 16:33:10 +0100

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -9,57 +9,41 @@ public class Graph {
     String[] keys;
     ArrayList<Integer>[] adjList;
 
-    public void crearGrafo(ArrayList<Autor> lista){
+    public void crearGrafo(Repositorio lista) {
         // Post: crea el grafo desde la lista de autores
         //       Los nodos son nombres de autores
 
-                // Paso 1: llenar th�
-        // COMPLETAR C�DIGO
-        th = new HashMap<>();
-        for(int i = 0; i < lista.size(); i++)
-            th.put(lista.get(i).getNombre(), i);
-
+        // Paso 1: llenar th
+        // COMPLETAR CÓDIGO
+        int id = 0;
+        for (Autor autor : lista.getListaAutores().values())
+            th.put(autor.getNombre(), id++);
 
         // Paso 2: llenar keys
         keys = new String[th.size()];
-        for (String k: th.keySet()) keys[th.get(k)] = k;
+        for (String nombre : th.keySet())
+            keys[th.get(nombre)] = nombre;
 
-        // Paso 3: llenar adjList�
-        // COMPLETAR C�DIGO
-        adjList = new ArrayList[th.size()];
-        for (int i = 0; i < adjList.length; i++) adjList[i] = new ArrayList<>();
-
-        // Construir relaciones basadas en co-autoría
-         HashMap<String, Autor> listaAutores= Repositorio.getRepositorio().getListaAutores();
-        for (Autor autor : lista) {
-            int indiceAutor = th.get(autor.getNombre());
-
-            // Obtener todas las publicaciones del autor
-            UnorderedDoubleLinkedList<Publicacion> publicaciones = autor.getListaPublicaciones();
-
-            // Para cada publicacion, obtener los co-autores
-            for (Publicacion pub : publicaciones) {
-                UnorderedDoubleLinkedList<Autor> autoresPub = pub.getListaAutores();
-
-                // Crear conexiones entre todos los autores de esta publicacion
-                for (Autor coAutor : autoresPub) {
-                    if (!coAutor.getNombre().equals(autor.getNombre())) {
-                        if (th.containsKey(coAutor.getNombre())) {
-                        int indiceCoAutor = th.get(coAutor.getNombre());
-
-                            // Añadir conexion bidireccional
-                            if (!adjList[indiceAutor].contains(indiceCoAutor)) {
-                                adjList[indiceAutor].add(indiceCoAutor);
-                            }
-                            if (!adjList[indiceCoAutor].contains(indiceAutor)) {
-                                adjList[indiceCoAutor].add(indiceAutor);
-                            }
-                        }
-                    }
-                }
+        // Paso 3: llenar adjList
+        // COMPLETAR CÓDIGO
+        adjList = (ArrayList<Integer>[]) new ArrayList[th.size()];
+        ArrayList<Integer> autoresPub = null;
+        for (Publicacion pub : lista.getListaPublicaciones().values()) {
+            autoresPub = new ArrayList<>();
+            for (Autor a : pub.getListaAutores()) {
+                if (th.containsKey(a.getNombre()))
+                    autoresPub.add(th.get(a.getNombre()));
             }
         }
-
+        // Añadir enlaces entre todos los autores de la publicación
+        for (int i = 0; i < autoresPub.size(); i++) {
+            for (int j = i + 1; j < autoresPub.size(); j++){
+                int x = autoresPub.get(i);
+                int y = autoresPub.get(j);
+                adjList[x].add(y);
+                adjList[y].add(x);
+            }
+        }
     }
 
     public void print(){
@@ -72,31 +56,12 @@ public class Graph {
     }
 
     public boolean estanConectados(String a1, String a2){
-        if (!th.containsKey(a1) || !th.containsKey(a2)) return false;
         Queue<Integer> porExaminar = new LinkedList<Integer>();
 
         int pos1 = th.get(a1);
         int pos2 = th.get(a2);
         boolean enc = false;
         boolean[] examinados = new boolean[th.size()];
-        // Si son el mismo autor
-        if (pos1 == pos2) return true;
-
-        porExaminar.add(pos1);
-        examinados[pos1] = true;
-
-        while (!porExaminar.isEmpty() && !enc) {
-            int actual = porExaminar.poll();
-            for (int i : adjList[actual]) {
-                if (!examinados[i]) {
-                    if (i==pos2){
-                        enc = true;
-                    }
-                    examinados[i] = true;
-                    porExaminar.add(i);
-                }
-            }
-        }
 
         // COMPLETAR CÓDIGO
 
@@ -112,7 +77,4 @@ public class Graph {
 
     }
 
-
 }
-
-
